7fe8cee32095d874fecbcba46971b3631c8a73a3
Timo Talvitie <timo.talvitie@vizucom.com>
2017-03-03 20:07:53 +0200

    Improve logging

--- git_batch_set_remotes.sh
+++ git_batch_set_remotes.sh
@@ -16,22 +16,29 @@ for d in */; do
       # Replace the beginning
       NEWREMOTE="git@bitbucket.org:vizucom${OLDSUFFIX}"
       git remote set-url origin ${NEWREMOTE}
-      echo "-Old remote:" ${OLDREMOTE}
-      echo "-New remote:" ${NEWREMOTE}
+      echo "-Updated remote to:" ${NEWREMOTE}
+      echo "-Old remote was:   " ${OLDREMOTE}
 
     elif [[ $OLDREMOTE == http*github.com/Vizucom* ]]
       then
         OLDSUFFIX=${OLDREMOTE#*/Vizucom}
         NEWREMOTE="git@github.com:Vizucom${OLDSUFFIX}"
         git remote set-url origin ${NEWREMOTE}
-        echo "-Old remote:" ${OLDREMOTE}
-        echo "-New remote:" ${NEWREMOTE}
+        echo "-Updated remote to:" ${NEWREMOTE}
+        echo "-Old remote was:   " ${OLDREMOTE}
     else
-        echo "-Not a Vizucom Github/Bitbucket repository"
+        if [[ $OLDREMOTE != *vizucom* ]] && [[ $OLDREMOTE != *Vizucom* ]]
+        then
+          echo "-Not a Vizucom Github/Bitbucket repository"
+        else
+          echo "-Already using SSH authentication"
+        fi
+
     fi
   else
-    echo "Not a git repository"
+    echo "-Not a git repository"
   fi
+  echo ""
   cd ..
 done
 
